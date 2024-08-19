1aca2be2c89425b1e7ba7e652bf96cdfdcc63a70
Fernando Silva <122708313+wonderfernando@users.noreply.github.com>
2024-08-19 07:08:18 +0100

    second commit

--- src/app/bot/[slag]/page.tsx
+++ src/app/bot/[slag]/page.tsx
@@ -18,7 +18,7 @@ const model = googleGenerativeAI.getGenerativeModel({ model: "gemini-1.5-flash" 
 
 
 const message_schemma = z.object({
-    message: z.string(),
+    message: z.string().optional(),
     sender: z.string().default("me"),
     id: z.string().default("")
 })
@@ -30,12 +30,14 @@ export default function BotMessagePage() {
     const [messages, setMessages] = useState<message_type[]>([])
     const [isLoading, setIsLoading] = useState(false)
     const [chatSession, setChatSession] = useState<ChatSession | null>(null)
+    const [hasFetch, setHashFetch] = useState(true)
+
     const { register, handleSubmit, setValue } = useForm<message_type>({
         resolver: zodResolver(message_schemma)
     })
 
-    async function run(prompt: string) {
-        let Messageresult = await chatSession!.sendMessage(prompt)
+    async function run(prompt: string | undefined) {
+        let Messageresult = await chatSession!.sendMessage(prompt!)
         const response = await Messageresult.response;
         const text = response.text();
         setMessages((state) => {
@@ -56,15 +58,25 @@ export default function BotMessagePage() {
     }
 
     useEffect(() => {
+        if (hasFetch) { setHashFetch(false); return; }
         (async () => {
             const chat = await model.startChat();
             setChatSession(chat)
-            const res = await chat?.sendMessage("voce agora é meu professor particular do ensino primario,estamos em uma aula, apenas eu e voce, e estamos estudando a disciplina de lingua portuguesa,estamos no inicio da aula entao pergunte sobre alguns topicos que eu gostaria de aprender, e quando estudarmos algum topico deixe sempre tarefas no final")
+            const res = await chat?.sendMessage("voce agora é meu professor particular do ensino primario,estamos em uma aula, apenas eu e voce, e estamos estudando a disciplina de Lingua portuguesa,estamos no inicio da aula entao pergunte sobre alguns topicos que eu gostaria de aprender, e quando estudarmos algum topico deixe sempre tarefas no final, fala com portugues de portugal")
+            const candidates = await res.response.candidates
 
-            console.log(await res.response.candidates)
+            if (candidates)
+                setMessages((state) => {
+                    const data: message_type | null = { id: crypto.randomUUID().toString(), message: candidates[0].content.parts[0].text, sender: "chat" };
+                    data.id = crypto.randomUUID().toString();
+                    return [...state, data]
+                })
+
         })()
-    }, [])
 
+    }, [hasFetch])
+
+    console.log(hasFetch)
     useEffect(() => {
         if (scrolldiv.current)
             scrolldiv.current.scrollTop = scrolldiv.current.scrollHeight;
@@ -73,7 +85,7 @@ export default function BotMessagePage() {
     return (
         <div className=" flex flex-col  h-[calc(100vh-40px)]">
             <div className="flex-1 overflow-y-auto p-4 flex items-start flex-col gap-2" ref={scrolldiv}>
-                {messages.map((message) => <Message key={message.id} sender={message.sender} message={message.message} />)}
+                {messages.map((message) => <Message key={message.id} sender={message.sender} message={message.message!} />)}
             </div>
             <form className="flex w-full px-4 pb-4" onSubmit={handleSubmit(handle_click)}>
                 <Input {...register("message")} className="w-full rounded-r-none focus-visible:ring-0" placeholder="Pergunte algo..." />
